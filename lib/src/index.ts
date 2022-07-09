a9e790b4bdfd139ec5744a0d71fc0ecfd4716e94
corno <corno.astn.a@schraverus.com>
2022-07-09 13:24:25 +0200

    .

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -2,3 +2,5 @@ export * from "./createDictionaryBuilder"
 export * from "./switchStatemenSupportingFunctions"
 export * from "./IDictionaryBuilder"
 export * from "./debug"
+
+export * from "pareto-api-core"
