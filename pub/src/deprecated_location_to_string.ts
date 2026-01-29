7934901cb6da7263b15abc1d9d26f802188c4418
Corno Schraverus <corno@schraverus.com>
2026-01-29 16:25:32 +0100

    .

--- pub/src/deprecated_location_to_string.ts
+++ pub/src/deprecated_location_to_string.ts
@@ -1,5 +1,5 @@
 import * as _pi from "./interface"
 
 export const _p_deprecated_location_to_string = ($: _pi.Deprecated_Source_Location): string => {
-    return `${$.file}:${$.line}:${$.column}`
+    return `${$['document resource identifier']}:${$.line}:${$.column}`
 }
