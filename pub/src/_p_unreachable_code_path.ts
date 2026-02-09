8ac487393215cba63236405347a1fa4a3c39f98c
Corno Schraverus <corno@schraverus.com>
2026-02-09 23:19:06 +0100

    pdt: ...

--- pub/src/_p_unreachable_code_path.ts
+++ pub/src/_p_unreachable_code_path.ts
@@ -1,3 +1,4 @@
+
 export default function _p_unreachable_code_path(explanation: string): never {
     throw new Error(`PANIC; this should have been an unreachable code path because ${explanation}`)
 }
