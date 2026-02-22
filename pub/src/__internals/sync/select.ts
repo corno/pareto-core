c5e476770a2bb0abb2ee7a3c0ad20fc7459a95bf
Corno Schraverus <corno@schraverus.com>
2026-02-22 12:17:28 +0100

    pdt: ...

--- pub/src/__internals/sync/select.ts
+++ pub/src/__internals/sync/select.ts
@@ -1,4 +1,5 @@
 import * as _pi from "../../interface"
+import * as _p from "./assign/literals/Optional"
 
 export const entry = <T>(
     dictionary: _pi.Dictionary<T>,
@@ -6,6 +7,20 @@ export const entry = <T>(
     abort: {
         no_such_entry: _pi.Abort<null>
     }
-) => {
-    return dictionary.__get_entry_deprecated(id, abort)
+): T => {
+    const entry = dictionary.__get_entry_raw(id)
+    return entry === null
+        ? abort.no_such_entry(null)
+        : entry[0]
 }
+
+export const possible_entry = <T>(
+    dictionary: _pi.Dictionary<T>,
+    id: string,
+): _pi.Optional_Value<T> => {
+    const entry = dictionary.__get_entry_raw(id)
+    return entry === null
+        ? new _p.Not_Set_Optional_Value<T>()
+        : new _p.Set_Optional_Value<T>(entry[0])
+}
+
