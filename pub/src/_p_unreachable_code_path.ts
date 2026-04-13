1a44e68e927f2c785fac9c4402310995fbdaf5f1
Corno Schraverus <corno@schraverus.com>
2026-04-13 09:27:01 +0200

    pdt: published version 0.1.68

--- pub/src/_p_unreachable_code_path.ts
+++ pub/src/_p_unreachable_code_path.ts
@@ -1,4 +1,10 @@
 
+export class Unreachable_Code_Path_Error extends Error {
+    constructor(explanation: string) {
+        super(`PANIC; this should have been an unreachable code path because ${explanation}`)
+    }
+}
+
 export default function _p_unreachable_code_path(explanation: string): never {
-    throw new Error(`PANIC; this should have been an unreachable code path because ${explanation}`)
+    throw new Unreachable_Code_Path_Error(explanation)
 }
--- pub/src/interface/algorithm_signatures/Production.ts
+++ pub/src/interface/algorithm_signatures/Production.ts
@@ -6,7 +6,7 @@ export type Production<Result, Error, Iterator_Item, End_Info> = (
     abort: Abort<Error>,
 ) => Result
 
-export type Production_With_Parameter<Result, Error, Iterator_Item, Parameter, End_Info> = (
+export type Production_With_Parameter<Result, Error, Iterator_Item, End_Info, Parameter> = (
     iterator: Iterator<Iterator_Item, End_Info>,
     abort: Abort<Error>,
     $p: Parameter,
@@ -16,7 +16,7 @@ export type Production_Without_Error<Result, Iterator_Item, End_Info> = (
     iterator: Iterator<Iterator_Item, End_Info>,
 ) => Result
 
-export type Production_Without_Error_With_Parameter<Result, Iterator_Item, Parameter, End_Info> = (
+export type Production_Without_Error_With_Parameter<Result, Iterator_Item, End_Info, Parameter> = (
     iterator: Iterator<Iterator_Item, End_Info>,
     $p: Parameter,
 ) => Result
