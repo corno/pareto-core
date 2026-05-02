3be44251c89a72841a069aa8aeb57c72c7b397d8
Corno Schraverus <corno@schraverus.com>
2026-05-02 08:40:46 +0200

    .

--- lib/src/command.ts
+++ lib/src/command.ts
@@ -1,4 +1,14 @@
-import * as _pi from  "./interface"
+import * as _pi from "./interface"
+
+import command_procedure from "./__internals/async/command_procedure"
+import __command_promise from "./__internals/async/command_promise"
+import __command from "./__internals/async/command"
+
+export {
+    command_procedure,
+    __command_promise,
+    __command
+}
 
 export * from "./__internals/sync/assign/decide" //for handling instructions
 export * from "./__internals/sync/extracts_for_async"
--- lib/src/query.ts
+++ lib/src/query.ts
@@ -1,3 +1,13 @@
+import query from "./__internals/async/query"
+import query_function from "./__internals/async/query_function"
+import __query_result from "./__internals/async/__query_result"
+
+export {
+    query,
+    query_function,
+    __query_result,
+}
+
 export * from "./__internals/sync/assign/decide"
 export * from "./__internals/sync/extracts_for_async"
 export * from "./__internals/async/query_expression"
