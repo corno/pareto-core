aa735e0e0f2ca30f25755cbee9cc15a949975b12
Corno Schraverus <corno@schraverus.com>
2026-05-02 19:44:41 +0200

    pdt: published version 0.1.84

--- typescript/lib/src/__internals/async/create_refinement_context.ts
+++ typescript/lib/src/__internals/async/create_refinement_context.ts
@@ -3,10 +3,10 @@ import * as _pi from  "../../interface"
 
 export type Refinement_Result<Output, Error> = {
 
-    __extract_data: (
-        on_success: ($: Output) => void,
-        on_error: ($: Error) => void
-    ) => void
+    __extract_data: <T>(
+        on_success: ($: Output) => T,
+        on_error: ($: Error) => T
+    ) => T
 
 }
 
@@ -14,17 +14,17 @@ export default function create_refinement_context <Output, Error>(
     callback: (abort: _pi.Abort<Error>) => Output,
 ): Refinement_Result<Output, Error> {
     return ({
-        __extract_data(
-            on_result: ($: Output) => void,
-            on_error: ($: Error) => void,
-        ): void {
+        __extract_data<T>(
+            on_result: ($: Output) => T,
+            on_error: ($: Error) => T,
+        ): T {
             class Refine_Guard_Abort_Error<Error> {
                 constructor(
                     public readonly error: Error,
                 ) { }
             }
             try {
-                on_result(
+                return on_result(
                     callback(
                         (error) => {
                             throw new Refine_Guard_Abort_Error(error);
@@ -33,7 +33,7 @@ export default function create_refinement_context <Output, Error>(
                 )
             } catch (e) {
                 if (e instanceof Refine_Guard_Abort_Error) {
-                    on_error(e.error)
+                    return on_error(e.error)
                 } else {
                     throw e // re-throw unexpected errors
                 }
