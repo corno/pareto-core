ead84e363cbb80c84c3c45b0592cf6e8f3c31eea
Corno Schraverus <corno@schraverus.com>
2026-06-14 11:16:57 +0200

    pdt: published version 0.1.121

--- typescript/lib/src/command.ts
+++ typescript/lib/src/command.ts
@@ -10,9 +10,36 @@ export {
     __command
 }
 
-export * from "./__internals/sync/assign/decide" //for handling instructions
 export * from "./__internals/sync/extracts_for_async"
 export * from "./__internals/async/command_procedure"
 export * from "./__internals/async/command_promise"
 export * from "./__internals/async/command"
 export * from "./__internals/async/command_statement"
+
+export namespace decide {
+
+    export type Option<T extends _pi.Value> = readonly [string, T]
+
+    export function ss<T extends _pi.Value, RT extends _pi.Command_Promise<any>>(
+        option: Option<T>,
+        $c: ($: T) => RT): RT {
+        return $c(option[1])
+    }
+
+    export function au<RT extends _pi.Command_Promise<any>>(
+        _x: never
+    ): RT {
+        throw new Error("unreachable")
+    }
+
+    export const state = <
+        T extends _pi.State,
+        RT extends _pi.Command_Promise<any>
+    >(
+        state: T,
+        assign: (output: T) => RT
+    ): RT => {
+        return assign(state)
+    }
+
+}
