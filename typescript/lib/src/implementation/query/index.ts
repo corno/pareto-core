b990a52987223b68460de5fc31ad731770a34fba
Corno Schraverus <corno@schraverus.com>
2026-06-16 11:59:19 +0200

    pdt: published version 0.1.151

--- typescript/lib/src/implementation/query/index.ts
+++ typescript/lib/src/implementation/query/index.ts
@@ -1,6 +1,6 @@
 
 export * as literal from "../__internal/sync/literal"
-export * from "./query_expressions"
+export * as e from "./query_expressions"
 export * from "./query_function"
 export * from "./query_result"
 export * from "./query"
