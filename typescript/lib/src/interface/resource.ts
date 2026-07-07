d1bcac8b3859c61cd1ae87b477dcb9ec2a343143
Corno Schraverus <corno@schraverus.com>
2026-07-07 10:29:07 +0200

    pdt: published version 0.1.248

--- typescript/lib/src/interface/resource.ts
+++ typescript/lib/src/interface/resource.ts
@@ -2,6 +2,6 @@ import { type Command } from './command.js'
 import { type Query } from './query.js'
 
 export type Resource = {
-    'commands': Record<string, Command<any, any>>,
-    'queries': Record<string, Query<any, any, any>>,
+    'commands': { [key:string]: Command<any, any> },
+    'queries': { [key:string]: Query<any, any, any> },
 }
