dc4cfcacbdac988df181315ded264e4fee1c6838
Corno Schraverus <corno@schraverus.com>
2026-07-12 13:29:04 +0200

    pdt: published version 0.1.263

--- typescript/lib/src/implementation/transformer.ts
+++ typescript/lib/src/implementation/transformer.ts
@@ -3,3 +3,5 @@ export * from "./__internal/sync/data_switch.js"
 export * as from from "./__internal/transformer/from.js"
 
 export * as literal from "./__internal/sync/literal.js"
+
+export * from "../interface/transformer.js"
