e727ad66da8db3b0f254d57e009b14001e71a874
Corno Schraverus <corno@schraverus.com>
2026-07-12 22:50:41 +0200

    pdt: published version 0.1.265

--- typescript/lib/src/interface/schemas.ts
+++
@@ -1,6 +0,0 @@
-
-export * from "./__internal/schemas/List.js"
-export * from "./__internal/schemas/Circular_Dependency.js"
-export * from "./__internal/schemas/Dictionary.js"
-export * from "./__internal/schemas/Optional_Value.js"
-export * from "./__internal/schemas/Value.js"
