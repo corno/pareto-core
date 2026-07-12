320332e45b2c7a8423f9e33e5bbbe19129671b33
Corno Schraverus <corno@schraverus.com>
2026-07-12 22:48:32 +0200

    pdt: published version 0.1.264

--- typescript/lib/src/implementation/refiner.ts
+++ typescript/lib/src/implementation/refiner.ts
@@ -2,3 +2,5 @@
 export * from "./__internal/sync/data_switch.js"
 export * as from from "./__internal/refiner/from.js"
 export * as literal from "./__internal/sync/literal.js"
+
+export * from "../interface/refiner.js"
