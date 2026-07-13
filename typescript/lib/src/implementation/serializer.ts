7ae41cc3109252b977620735a1cfd92e2628b774
Corno Schraverus <corno@schraverus.com>
2026-07-13 23:04:17 +0200

    pdt: published version 0.1.272

--- typescript/lib/src/implementation/serializer.ts
+++ typescript/lib/src/implementation/serializer.ts
@@ -6,6 +6,8 @@ export * as from from "./__internal/transformer/from.js"
 
 export * as literal from "./__internal/sync/literal.js"
 import { type Phrase, type Paragraph } from "../temp/fountain_pen/prose.js"
+import * as fp_serialize from "../temp/fountain_pen/serializers/prose.js"
+import p_text_from_list from "../implementation/transformer/specials/text_from_list.js"
 
 export type Phrase_Serializer<
     Input extends Value,
@@ -61,3 +63,16 @@ export type Paragraph_Serializer_With_Parameter<
 //     const raw = list.__get_raw().map(($) => assign_character($))
 //     return { raw }
 // }
+
+export const phrase_to_text = ($: Phrase, indentation: string, newline: string): string => {
+    return p_text_from_list(
+        fp_serialize.Phrase(
+            $,
+            {
+                'newline': newline,
+                'indentation': indentation,
+            }
+        ),
+        ($) => $
+    )
+}
