b194ee47f93e62f0b508e3b7971e5e100a534633
Corno Schraverus <corno@schraverus.com>
2026-07-15 11:51:52 +0200

    pdt: published version 0.1.281

--- typescript/lib/src/implementation/serializer.ts
+++ typescript/lib/src/implementation/serializer.ts
@@ -1,4 +1,5 @@
 import { type Value } from "../interface/__internal/schema/Value.js"
+import { type List } from "../interface/__internal/schema/List.js"
 
 export * from "./__internal/sync/data_switch.js"
 
@@ -7,6 +8,8 @@ export * as from from "./__internal/transformer/from.js"
 export * as literal from "./__internal/sync/literal.js"
 import { type Phrase } from "../temp/fountain_pen/phrase.js"
 import * as data_switch from "./__internal/sync/data_switch.js"
+
+import p_text_from_list from "./transformer/specials/text_from_list.js"
 
 export type Phrase_Serializer<
     Input extends Value,
@@ -32,8 +35,24 @@ export const text_from_phrase = (
 
     switch ($[0]) {
         case 'value': return $[1] // return the string directly
-        case 'composed': return $[1].__get_raw().map(($) => text_from_phrase($)).join("")
+        case 'composed': return $[1].map(($) => text_from_phrase($)).join("")
         case 'nothing': return ""
         default: return data_switch.exhaustive($[0])
     }
+}
+export namespace ph {
+    export const composed = (
+        phrases: Phrase[]
+    ): Phrase => ['composed', phrases]
+
+    export const literal = (
+        value: string
+    ): Phrase => ['value', value]
+
+    export const list_of_characters = (
+        list: List<number>
+    ): Phrase => ['value', p_text_from_list(
+        list,
+        ($) => $,
+    )]
 }
--- typescript/lib/src/temp/fountain_pen/phrase.ts
+++ typescript/lib/src/temp/fountain_pen/phrase.ts
@@ -1,8 +1,7 @@
 
-import * as p_ from '../../interface/schema.js'
 
 export type Phrase = 
     string
     | readonly ['value', string]
-    | readonly ['composed', p_.List<Phrase>]
+    | readonly ['composed', readonly Phrase[]]
     | readonly ['nothing', null]
